d7eab3e27615c8d9936c6561e80d4ffefce09147
Veronica Kim <vero.a.kim@gmail.com>
2017-11-29 03:55:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,27 @@
 function forLoop(array) {
 for (let i = 0; i < 25; i++) {
   if (i === 1) {
-    console.log("I am 1 strange loop.");
+    array.push("I am 1 strange loop.");
   } else {
-    console.log("I am ${i} strange loops.");
-  } return array;
+    array.push(`I am ${i} strange loops.`);
+  }
 }
+return array;
 }
+
+function whileLoop(number) {
+  // var i = 0
+  while (number != 0) {
+  console.log(number);
+  number--
+}
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  return Math.random() >= 0.5
+}
+  do {
+    console.log(array)
+   }while (array.length > 0 && maybeTrue());
+  return array;
